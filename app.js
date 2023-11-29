419c20b3e2b72d7272d0702c96cf8699902d043d
ravikant706 <ravikant706@gmail.com>
2023-11-30 01:02:06 +0530

    home.ejs page layout with header.ejs and footer.ejs

--- app.js
+++ app.js
@@ -15,8 +15,14 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+
+
+
+
+
 app.get('/', function(req, res){
-  res.render("home");
+  res.render("home", {StartingContent: homeStartingContent});
 })
 
 
