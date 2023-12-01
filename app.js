14b7856f39da5534c22297061883dc06579a2479
ravikant706 <ravikant706@gmail.com>
2023-12-01 21:54:31 +0530

    when we post any thing in compose file data go to blog page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui 
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
+
 const app = express();
 
 app.set('view engine', 'ejs');
@@ -15,14 +16,15 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-
-
-
+let posts = [];  // Global veriable/Empty array
 
 
 
 app.get('/', function(req, res){
-  res.render("home", {StartingContent: homeStartingContent});
+  res.render("home", {
+    StartingContent: homeStartingContent,
+      posts: posts});
+  // console.log(posts); //data come from res.redirect root and save in server
 })
 
 app.get('/about', function(req, res){
@@ -37,9 +39,17 @@ app.get('/compose', function(req, res){
   res.render('compose');
 })
 
+// data come from compose file and go home file and res.redirect() post data in global veriabal
 app.post('/compose', function(req, res){
-  console.log(req.body.items)  //
-})
+  // console.log(req.body.postTitle)  //data goes to hyper
+  const post = {
+    title : req.body.postTitle,
+    contant : req.body.postBody
+  };
+
+  posts.push(post); // data go to global or empty veriable and then go in home page
+  res.redirect('/');
+});
 
 
 
